94a07de00bbb33c7744e7a631d2d89551df13be9
Om <36092598+Khetwal-om@users.noreply.github.com>
2020-11-01 12:47:49 +0530

    Required dependencies are installed

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+  apiKey: 'AIzaSyAd1yYTPbdUiaGGWyartaecr_nzfC03lQk',
+  authDomain: 'todosovercomplicated.firebaseapp.com',
+  databaseURL: 'https://todosovercomplicated.firebaseio.com',
+  projectId: 'todosovercomplicated',
+  storageBucket: 'todosovercomplicated.appspot.com',
+  messagingSenderId: '301788317919',
+  appId: '1:301788317919:web:b7df6f2c95e8c9bb6c816a'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
